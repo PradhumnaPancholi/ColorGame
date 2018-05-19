1f0d17dc24de721805620809e8c99a3645f06942
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-05-19 19:12:00 -0400

    behavious on clicking wrong and correct square

--- colorGame.js
+++ colorGame.js
@@ -9,9 +9,11 @@ var colors = [
 //selescting square div(s)
 var square = document.querySelectorAll(".square");
 //variable for selected color
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 //var to change picked color(question) in header
 var colorDisplay = document.getElementById("colorDisplay");
+//for maniupilation of display board//
+var messageDisplay = document.getElementById("message");
 
 //manuplating picked color in header
 colorDisplay.textContent = pickedColor; 
@@ -27,11 +29,28 @@ for (var i = 0;  i < square.length; i++){
         var clickedColor = this.style.backgroundColor;
         //comparing picked color with clicked color
         if(clickedColor === pickedColor){
-            alert("Correct");
+            messageDisplay.textContent = "Correct";
+            changeColor(clickedColor);
         }else{
-            alert("Wrong, try again");
+            //behavious for wrong answer//
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
         }
 
     });
+
+//function for the behaviour on correct answer//
+function changeColor(color){
+    //loop to affect all square according to correct answer//
+    for (var i = 0; i < square.length; i++){
+        //change each color to correct color//
+        square[i].style.backgroundColor = color;
+    }
+}
+
+//function to pick random color
+function pickColor(){
+    Math.random()
+}
    
 } 
