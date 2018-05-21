c37c2431ff908ab3b5024cf8e7b763d9d6cc12bc
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-05-21 14:28:38 -0400

    added easy and hard mode

--- colorGame.js
+++ colorGame.js
@@ -1,4 +1,5 @@
-var colors = generateRandoomColors(6);
+var numOfSquares = 6;
+var colors = generateRandoomColors(numOfSquares);
 //selecting square div(s)
 var square = document.querySelectorAll(".square");
 //variable for selected color
@@ -9,10 +10,43 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
 var h1 = document.querySelector("h1");
 var resetButton = document.getElementById("reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+//easy button//
+easyBtn.addEventListener("click", function(){
+    easyBtn.classList.add("selected");
+    hardBtn.classList.remove("selected");
+    numOfSquares = 3;
+    colors = generateRandoomColors(numOfSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < square.length; i++){
+        if(colors[i]){
+        square[i].style.backgroundColor = colors[i];
+        } else{
+          square[i].style.display = "none";  
+        }
+    }
+});
+
+//hard button//
+hardBtn.addEventListener("click", function(){
+    easyBtn.classList.remove("selected");
+    hardBtn.classList.add("selected");
+    numOfSquares = 6;
+    colors = generateRandoomColors(numOfSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for(var i = 0; i < square.length; i++){
+        square[i].style.backgroundColor = colors[i];
+        square[i].style.display = "block";  
+    }
+});
 
 resetButton.addEventListener("click", function(){
     //generate new color//
-    colors = generateRandoomColors(6);
+    colors = generateRandoomColors(numOfSquares);
     // pick a new random color//
     pickedColor =  pickColor();
     //change color display to match picked color//
